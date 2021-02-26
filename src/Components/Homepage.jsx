3148d620e06ba7bc68b96294991efb630a36a87d
Sam Kessler <sholomkessler1@gmail.com>
2021-02-26 12:50:30 -0500

    Create Homepage.jsx

---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Homepage() {
+	return (
+		<div>
+			<img class="profilephoto" src="Images/profile.jpg" alt="Mugshot required." />
+			<h1>Sam Kessler</h1>
+			<h3>Unity Gameplay Programmer</h3>
+		</div>
+	);
+}
+
+export default Homepage;
