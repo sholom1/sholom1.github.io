eafdd5ccd8b135c267680e827825ded2fbbc8b04
Sam Kessler <sholomkessler1@gmail.com>
2021-02-26 16:44:19 -0500

    Fixed flex issue

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -5,21 +5,25 @@ import './Navbar.css';
 function Navbar() {
 	return (
 		<div className="container">
-			<Link className="link" to="/">
-				Home
-			</Link>
-			<Link className="link" to="/contact">
-				Contact
-			</Link>
-			<Link className="link" to="/projects">
-				Projects
-			</Link>
-			<Link className="link" to="/projects/XavianHathor">
-				Xavian {'&'} Hathor
-			</Link>
-			<Link className="link" to="/projects/ElectionMapbox">
-				Election Mapbox Local Demo
-			</Link>
+			<div className="rightContainer">
+				<Link className="link" to="/">
+					Home
+				</Link>
+				<Link className="link" to="/contact">
+					Contact
+				</Link>
+				<Link className="link" to="/projects">
+					Projects
+				</Link>
+				<Link className="link" to="/projects/XavianHathor">
+					Xavian {'&'} Hathor
+				</Link>
+				<Link className="link" to="/projects/ElectionMapbox">
+					Election Mapbox Local Demo
+				</Link>
+			</div>
+			<div className="leftContainer"></div>
 		</div>
 	);
 }
+export default Navbar;
